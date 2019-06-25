ce405d28539dc611fa75047b7dbbbd248cef90a2
evan999 <emeshberge@gmail.com>
2019-06-24 21:13:50 -0400

    Navigates to portfolio page. Start scraping stock data.

--- FinanceScraper_ConsoleApp/Program.cs
+++ FinanceScraper_ConsoleApp/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
+using System.Threading;
 
 namespace FinanceScraper_ConsoleApp
 {
@@ -15,40 +16,47 @@ namespace FinanceScraper_ConsoleApp
         {
             IWebDriver driver = new ChromeDriver();
 
-            driver.Navigate().GoToUrl("http://www.finance.yahoo.com/");
+            driver.Navigate().GoToUrl("https://finance.yahoo.com/");
 
-            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            WebDriverWait waitLogin = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            waitLogin.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.Id("uh-signedin")));
 
-            // driver.FindElement(By.Id("email")).SendKeys("userName");
+            IWebElement loginButton = driver.FindElement(By.Id("uh-signedin"));
+            loginButton.Click();
+
+            WebDriverWait waitEnterUsername = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            waitEnterUsername.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.Id("login-username")));
+
+            IWebElement userName = driver.FindElement(By.Id("login-username"));
+
+            userName.SendKeys("meshberge");
+            userName.SendKeys(Keys.Enter);
+
+            WebDriverWait waitEnterPassword = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            waitEnterPassword.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.Id("login-passwd")));
+            IWebElement password = driver.FindElement(By.Id("login-passwd"));
+
+            password.SendKeys("toonfan1!");
+            password.SendKeys(Keys.Enter);
+
+            driver.Navigate().GoToUrl("https://finance.yahoo.com/portfolio/p_1/view/v1");
+
+            // WebDriverWait waitDataTable = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            // waitDataTable.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.ClassName("W(100 %)")));
+            // IWebElement dataTable = driver.FindElement(By.ClassName("W(100 %)"));
+
+
+            
+            // WebDriverWait waitStocks = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            // waitStocks.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.Id("login-passwd")));
+
+
             /*
-            using (IWebDriver driver = new ChromeDriver())
+            for (int stock = 0; stock < 10; stock++)
             {
-                //Notice navigation is slightly different than the Java version
-                //This is because 'get' is a keyword in C#
-                driver.Navigate().GoToUrl("http://www.finance.yahoo.com/");
 
-
-
-                // Find the text input element by its name
-                // IWebElement query = driver.FindElement(By.Name("q"));
-
-                // Enter something to search for
-                //query.SendKeys("Cheese");
-
-                // Now submit the form. WebDriver will find the form for us from the element
-                //query.Submit();
-
-                // Google's search is rendered dynamically with JavaScript.
-                // Wait for the page to load, timeout after 10 seconds
-                var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
-                //wait.Until(d => d.Title.StartsWith("cheese", StringComparison.OrdinalIgnoreCase));
-
-                // Should see: "Cheese - Google Search" (for an English locale)
-                //Console.WriteLine("Page title is: " + driver.Title);
-                
             }
             */
-
         }
     }
 }
